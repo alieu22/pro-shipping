24859d3c5197e8b2322905fd6d8ef2298f1b5540
sainabouc <csainabou785@gmail.com>
2023-07-01 16:14:48 -0700

    My first edit

--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -4,6 +4,15 @@
 
 <section> 
     <h1>This is the services page</h1>
+    <div>
+        <p>We provide services to many different countries.</p>
+    </div>
+    <style>
+         h1 {
+            color: blue;
+         }  
+    </style>
 </section>
+
 
 @endsection
