048af7cc0e54ba777811e5e288b6cae96cb9df55
alieu22 <alieusma@gmail.com>
2023-07-09 15:16:29 -0700

    Added some testing text

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -5,7 +5,7 @@
 <section> 
     <h1>This is the Contact page</h1>
 
-<p> This is where you will want to contact me <p>
+<p> This is where you will want to contact me for inquries <p>
 </section>
 
 @endsection
