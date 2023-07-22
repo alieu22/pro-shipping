f97d5a6b45ccfae507439afda700315d8260ab88
alieu22 <alieusma@gmail.com>
2023-07-22 15:06:11 -0700

    cleanup pt 2

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -3,26 +3,21 @@
 
 @section('content')
 
-<section>
-  <div class="container-fluid bg-info p-2" style="max-height: 165px;">
+<section> 
+  <div class="container-fluid bg-info p-2 mt-n4" style="max-height: 120px;">
     <div class="row align-items-end" style="height: 100%;">
       <div class="col-md-8 col-lg-6 align-self-start">
-        <h1 style="margin-top: 10px;">About us</h1>
+        <h1 style="margin-bottom: 10px;">About us</h1>
         <h4>"Eat sleep, meet our customers needs, repeat" - Pro Shipping</h4>
       </div>
       <div class="col-md-4 col-lg-6 d-flex justify-content-end align-items-center">
-        <img src="{{ asset('img/Driver.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 25%; margin-right: 150px; margin-bottom: 10px;">
+        <img src="{{ asset('img/Driver.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 25%; margin-right: 120px; margin-bottom: 10px;">
       </div>
     </div>
     <div class="row">
-    <div class="col-md-4 col-lg-6 d-flex justify-content-start align-items-center">
-  <img src="{{ asset('img/Driver2.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 30%; margin-right: 20px; margin-top: 30px;">
-  
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et elit quis odio consequat semper vitae a urna. 
-    Donec at mollis magna. Curabitur in sapien venenatis, tristique libero nec, volutpat lorem.
-     Nullam pretium elit vel felis placerat venenatis. Suspendisse tincidunt lectus sit amet convallis pretium</p>
-</div>
-
+      <div class="col-md-4 col-lg-6 d-flex justify-content-start align-items-center">
+        <img src="{{ asset('img/Driver2.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 30%; margin-right: 20px; margin-top: 30px;">
+      </div>
     </div>
     <div class="row">
       <div class="col-md-6">
--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -5,24 +5,20 @@
 @section('content')
 
 <section> 
-
-  <div class="container-fluid bg-info p-2" style="max-height: 165px;">
+  <div class="container-fluid bg-info p-2 mt-n4" style="max-height: 120px;">
     <div class="row align-items-end" style="height: 100%;">
       <div class="col-md-8 col-lg-6 align-self-start">
-        <h1 style="margin-top: 10px;">Our Services</h1>
+        <h1 style="margin-bottom: 10px;">Our Services</h1>
         <h4>"Eat sleep, meet our customers needs, repeat" - Pro Shipping</h4>
       </div>
       <div class="col-md-4 col-lg-6 d-flex justify-content-end align-items-center">
-        <img src="{{ asset('img/Driver.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 25%; margin-right: 150px; margin-bottom: 10px;">
+        <img src="{{ asset('img/Driver.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 25%; margin-right: 120px; margin-bottom: 10px;">
       </div>
     </div>
     <div class="row">
-    <div class="col-md-4 col-lg-6 d-flex justify-content-start align-items-center">
-  <img src="{{ asset('img/Driver2.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 30%; margin-right: 20px; margin-top: 30px;">
-  
-  
-</div>
-
+      <div class="col-md-4 col-lg-6 d-flex justify-content-start align-items-center">
+        <img src="{{ asset('img/Driver2.jpeg') }}" alt="Ps-picture" class="img-fluid rounded" style="max-width: 30%; margin-right: 20px; margin-top: 30px;">
+      </div>
     </div>
     <div class="row">
       <div class="col-md-6">
@@ -30,9 +26,8 @@
       </div>
     </div>
   </div>
-  
-
 </section>
+
 
 
 @endsection
