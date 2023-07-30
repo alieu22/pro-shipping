9ae308d159ccd4788939c2d69fe322908e606e6c
alieu22 <alieusma@gmail.com>
2023-07-29 22:13:19 -0700

    Cleanup 1.2

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -5,12 +5,6 @@
 
 
 <section> 
-<!DOCTYPE html>
-<html>
-<head>
-  
-</head>
-<body>
     <section>
         <div class="custom-container">
             <div class="row align-items-end">
@@ -21,38 +15,40 @@
             </div>
         </div>
 
-        <div class="Contact-Form"> 
-         <label class = "ps-labels" for="fullname">Name</label>
+       
+        <form class="hf-form">
+        <div class="Contact-Form">
+                <label class="ps-labels" for="fullname">Name</label>
                 <br>
-                <input class = "ps-box" type="text" placeholder = "Enter your name" pattern="[a-zA-Z ]{1,30}" id = "fullname" name="full_name" title="(required)" required>
-            </div>
-
-            <div class = "Contact-Form">
-                <label class = "ps-Labels" for="phonenumber"> Phone Number</label>
-                <br>
-                <input class = "ps-box" type="text" placeholder = "Enter your phone number"   pattern="^\d{3}-\d{3}-\d{4}$" id = "phonenumber" name="phone" title="(123-456-7890)" required>
-            </div>
-
-            <div class = "Contact-Form">
-                <label class = "ps-Labels" for="email">Email </label>
-                
-                <input class = "ps-box" type="text" placeholder = "Enter your email"  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" id = "email" title="(required)" name="email" required >
+                <input class="ps-box" type="text" placeholder="Enter your name" pattern="[a-zA-Z ]{1,30}" id="fullname" name="full_name" title="(required)" required>
             </div>
 
             <div class="Contact-Form">
-             <label class="ps-Labels" for="comments">Reason for contact *</label>
-             <input class = "ps-box" type="text" placeholder = "Enter your reason"  pattern="[a-zA-Z0-9
-             
+                <div>
+                    <label class="ps-Labels" for="email">Email</label>
+                </div>
+                <input class="ps-box" type="text" placeholder="Enter your email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" id="email" title="(required)" name="email" required>
+            </div>
 
-                <div class = "ps-submit">
-                 <input class = "ps-submitbtn" type="submit">
-             </div>
+            <div class="Contact-Form">
+                <div>
+                    <label class="ps-Labels" for="tracking number">Tracking Number(If applicable)</label>
+                </div>
+                <input class="ps-box" type="text" placeholder="Enter your tracking number" pattern="^\d{3}-\d{3}-\d{4}$" id = "phonenumber" name="phone" title="(123-456-7890)" required>
+            </div>
 
-    </section>
-</body>
-</html>
+            <div class="Contact-Form">
+                <div>
+                    <label class="ps-Labels" for="comments">Reason for contact *</label>
+                </div>
+                <input class="ps-box" type="text" placeholder="Enter your reason" pattern="[a-zA-Z0-9]" id="comments" name="comments" required>
+            </div>
 
-
+            <div class="hf-center">
+                <input type="submit" class="hf-submitbtn" value="Submit">
+            </div>
+        </form>
+    </div>
 </section>
 
 
