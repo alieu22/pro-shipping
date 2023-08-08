2d4493887e6c2ed3b092ddb135a844fe068993cc
sainabouc <csainabou785@gmail.com>
2023-08-08 14:15:28 -0700

    Fixed the about page

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -2,38 +2,12 @@
 @extends('main')
 
 @section('content')
-<<<<<<< HEAD
 <div class="custom-container">
     <div class="col-md-8 col-lg-6 cust-cont">
         <h1 style="margin-bottom: 10px;">About Us</h1>
         <h4>"Eat sleep, meet our customers needs, repeat" - Pro Shipping</h4>
         <div class="image">
             <img src="{{ asset('img/deliveryman.jpg') }}" alt="delivery man">
-=======
-<section> 
-<!DOCTYPE html>
-<html>
-<head>
-  
-</head>
-<body>
-    <section>
-        <div class="custom-container">
-            <div class="row align-items-end">
-                <div class="col-md-8 col-lg-6 align-self-start">
-                    <h1 style="margin-bottom: 10px;">About Us</h1>
-                    <h4>"Eat sleep, meet our customers needs, repeat" - Pro Shipping</h4>
-                <div class="first-section">
-                    <img class= "about-img1"src="{{ asset('img/deliveryman.jpg') }}" alt="delivery man">
-                </div>
-                <div class="second-section">
-                    <img class="about-img2" src="{{ asset('img/deliveryguywithcustomer.jpg') }}" alt="delivery">
-                </div>
-               
-                
-                </div>
-            </div>
->>>>>>> 9ae308d159ccd4788939c2d69fe322908e606e6c
         </div>
     </div>
 </div>
