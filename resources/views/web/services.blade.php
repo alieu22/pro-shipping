ffd9b664fe2da731476c69268222695092e3e519
Fatima Camara <fatimalamin73@gmail.com>
2023-08-10 20:37:08 -0700

    updated services page

--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -13,7 +13,34 @@
     </div>
 </div>
 
-<div class="page-cont">
+<div class="services-page">
+
+    <div>
+        <div>
+            <button type="button">Shipping</button>
+
+            <p>Ship within the US, Africa, Asia, Europe, Middle East etc. and recieve your package in no time.</p>
+
+            <p>Experience a seamless and top notch shipping experience when you ship with Pro Shipping. <br> 
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aperiam tempora natus velit <br>
+             culpa aut modi eum cumque, sapiente mollitia inventore exercitationem voluptate officia quidem <br>
+              expedita tempore placeat. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi fuga, <br> commodi velit, sunt ut dolorem cumque molestias suscipit doloremque deleniti ipsum iusto cum <br> unde odit quibusdam. Architecto, tempora repellendus. Quasi. Lorem ipsum dolor sit amet 
+            </p>
+        
+        </div>
+        
+        
+            <div class= "images">
+                <img  src="{{ asset('img/shippingbox.jpg') }}" alt="boxes">
+                <img  src="{{ asset('img/mansigningbox.jpg') }}" alt="signing box">
+                <img  src="{{ asset('img/trucks.jpg') }}" alt="trucks">
+
+            </div>
+
+            
+        </div>
+    </div>
+    
     <!--- Page Content needs to go here --->
 </div>
 
