520923a01729445864599d2eafbe41837a30a632
Fatima Camara <fatimalamin73@gmail.com>
2023-08-15 15:38:31 -0700

    me and fatoumata added a fourth image, aligned text and button, bolded locations, changed the color of the button

--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -17,15 +17,15 @@
 
     <div>
         <div>
-            <button type="button">Shipping</button>
+            <button class="services-button" type="button">Shipping</button>
 
-            <p>Ship within the US, Africa, Asia, Europe, Middle East etc. and recieve your package in no time.</p>
+            <p>Ship within the <strong>US</strong>, <strong>Africa</strong>, <strong>Asia</strong>,<br>
+             <strong>Europe</strong>, <strong>Middle East</strong> etc. and<br>
+              recieve your package in no time.</p>
 
-            <p>Experience a seamless and top notch shipping experience when you ship with Pro Shipping. <br> 
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci aperiam tempora natus velit <br>
-             culpa aut modi eum cumque, sapiente mollitia inventore exercitationem voluptate officia quidem <br>
-              expedita tempore placeat. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi fuga, <br> commodi velit, sunt ut dolorem cumque molestias suscipit doloremque deleniti ipsum iusto cum <br> unde odit quibusdam. Architecto, tempora repellendus. Quasi. Lorem ipsum dolor sit amet 
-            </p>
+            <p>Experience a seamless and top<br>
+             notch shipping experience when<br>
+              you ship with Pro Shipping.</p>
         
         </div>
         
@@ -34,6 +34,8 @@
                 <img  src="{{ asset('img/shippingbox.jpg') }}" alt="boxes">
                 <img  src="{{ asset('img/mansigningbox.jpg') }}" alt="signing box">
                 <img  src="{{ asset('img/trucks.jpg') }}" alt="trucks">
+                <img  src="{{ asset('img/airplane.jpg') }}" alt="airplane">
+
 
             </div>
 
