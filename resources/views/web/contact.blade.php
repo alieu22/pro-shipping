f08aa0fad8fe30802e23a04a3caab679b90bbea8
alieu22 <alieusma@gmail.com>
2023-08-22 17:10:43 -0700

    did some contact page cleanup

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -1,6 +1,45 @@
 @extends('main')
 
 @section('content')
+<section class="hf-sections">
+    <div class="hf-contactMain">
+        <!--<h1 class="h-first hf-contact-header">Interested in Contacting us?</h1> -->
+       <p class="hf-contact-para">For any questions regarding Pro Shipping,<br> please complete this form.</p> 
+
+        <form class="hf-form" action="" method="post">
+            @csrf
+
+            <div class="hf-name">
+                <label class="hf-Labels" for="fullname">Name * (John Doe)</label>
+                <input class="hf-box" type="text" placeholder="Enter your name" pattern="[a-zA-Z ]{1,30}" id="fullname" name="full_name" title="(required)" required>
+            </div>
+
+            <div class="hf-number">
+                <label class="hf-Labels" for="phonenumber"> Tracking Number * (123-456-7890)</label>
+                <input class="hf-box" type="tel" placeholder="Enter your Tracking number" pattern="^\d{3}-\d{3}-\d{4}$" id="phonenumber" name="phone" title="(123-456-7890)" required>
+            </div>
+
+            <div class="hf-email">
+                <label class="hf-Labels" for="email">Email * (abc@email.com)</label>
+                <input class="hf-box" type="email" placeholder="Enter your email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" id="email" title="(required)" name="email" required>
+            </div>
+
+            <div class="hf-comments">
+                <label class="hf-Labels" for="comments">Reason for contact *</label>
+                <textarea class="comment-box" rows="5" name="reason" required></textarea>
+            </div>
+
+            <div class="hf-submit">
+                <input class="hf-submitbtn" type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
+</section>
+
+
+
+
+<!-- uncomment if the new code insertion does not work out 
 <section class = "hf-sections">
     <div class = "hf-contactMain">
         <h2 class="hf-he hf-contact-header hf-center">Interested in Contacting us? </h2>
@@ -39,4 +78,6 @@
         </form>
     </div>
 </section>
+
+-->
 @endsection
