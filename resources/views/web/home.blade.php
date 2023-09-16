7b140f9cdb935fe815a7ef2f2ccbfeda87a01589
alieu22 <alieusma@gmail.com>
2023-09-16 15:01:14 -0700

    continued footer work

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,12 +1,10 @@
-
 @extends('main') <!-- layouts.app !-->
 
 @section('content')
-<body class = "home-color">
 <div class="custom-container">
     <div class="col-md-8 col-lg-6 cust-cont">
         <h1 style="margin-bottom: 10px;">Home</h1>
-        <h4>"Eat sleep, meet our customers needs, repeat" - Pro Shipping</h4>
+        <h4>"Eat sleep, meet our customers' needs, repeat" - Pro Shipping</h4>
         <div class="image">
             <img src="{{ asset('img/deliveryman.jpg') }}" alt="delivery man">
         </div>
@@ -14,45 +12,35 @@
 </div>
 
 <div class="page-cont">
-    <!--- Page Content needs to go here --->
- 
-
-
-<div class="home-content"> 
- <h1 class="welcome-text"> Welcome to Pro Shipping </h1>
- <p class="home-paragraph"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip<br> ex ea commodo consequat. Duis aute irure 
-    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-    proident, sunt in culpa qui officia deserunt <br> mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco <br>
-    
-<p>
-</div>
-
-
-<div class="home-gallery">
-    
-    <div class="homeimg-1">
-        <img src="{{ asset('img/airfreight.jpg') }}" alt="airplane">
-        <p> The best way to ship! Ship your items with Pro shipping <br> today and recieve them in no time <p>
-        <a href="{{ route('products') }}" class="view-more1">View More</a>
+    <div class="home-content"> 
+        <h1 class="welcome-text">Welcome to Pro Shipping</h1>
+        <p class="home-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+        </p>
     </div>
 
-    <div class="homeimg-2">
-        <img src="{{ asset('img/image2.jpg') }}" alt="deliveryman">
-        <p> The best way to ship! Ship your items with Pro shipping <br> today and recieve them in no time <p>
-        <a href="{{ route('services') }}" class="view-more2">View More</a>
-    </div>
-    <div class="homeimg-3">
-        <img src="{{ asset('img/image3.jpg') }}" alt="delivercustomer">
-        <p> The best way to ship! Ship your items with Pro shipping today <br> and recieve them in no time <p>
-        <a href="{{ route('contact') }}" class="view-more3">View More</a>
+    <div class="home-gallery">
+        <div class="homeimg-1">
+            <img src="{{ asset('img/airfreight.jpg') }}" alt="airplane">
+            <p>The best way to ship! Ship your items with Pro shipping today and receive them in no time.</p>
+            <a href="{{ route('products') }}" class="view-more1">View More</a>
+        </div>
+
+        <div class="homeimg-2">
+            <img src="{{ asset('img/image2.jpg') }}" alt="deliveryman">
+            <p>The best way to ship! Ship your items with Pro shipping today and receive them in no time.</p>
+            <a href="{{ route('services') }}" class="view-more2">View More</a>
+        </div>
+        <div class="homeimg-3">
+            <img src="{{ asset('img/image3.jpg') }}" alt="delivercustomer">
+            <p>The best way to ship! Ship your items with Pro shipping today and receive them in no time.</p>
+            <a href="{{ route('contact') }}" class="view-more3">View More</a>
+        </div>
     </div>
 </div>
-
-</div>
-
-</body>
 @endsection
 
 
--- resources/views/web/products.blade.php
+++ resources/views/web/products.blade.php
@@ -1,46 +1,35 @@
-<!-- the bootstrap styles will be applied to this page , refer to the documentation for borrowing the code !-->
 @extends('main')
 
 @section('content')
 <div class="custom-container">
     <div class="col-md-8 col-lg-6 cust-cont">
         <h1 style="margin-bottom: 10px;">Products</h1>
-        <h4>"Eat sleep, meet our customers needs, repeat" - Pro Shipping</h4>
+        <h4>"Eat sleep, meet our customers' needs, repeat" - Pro Shipping</h4>
         <div class="image">
             <img src="{{ asset('img/deliveryman.jpg') }}" alt="delivery man">
-    </div>
+        </div>
     </div>
 </div>
 
-<div class="products">
-    <div class="productspage">
-        <p>We ship envelopes and packages<br> weighing up to 150lbs throughout <br>the <strong>US, Africa, Asia, Europe, <br>Middle East</strong> etc.</p>
-        <!--- Page Content needs to go here --->
-        <p> Ship worldwide with Pro shipping<br> and get your package in no time<br> with Pro Shipping!</p>
+<div class="page-cont">
+    <div class="products-page">
+        <p>We ship envelopes and packages<br>weighing up to 150lbs throughout<br>the <strong>US, Africa, Asia, Europe,<br>Middle East</strong>, etc.</p>
+        <p>Ship worldwide with Pro shipping<br>and get your package in no time<br>with Pro Shipping!</p>
     </div>
 
-    <div class="images-container">
-        
-            <div class="product-img1">
+    <div class="images">
+        <div class="product-img-1">
             <img src="{{ asset('img/shippingbox.jpg') }}" alt="boxes">
-</div>
-
-        <div class="product-img2">
+        </div>
+        <div class="product-img-2">
             <img src="{{ asset('img/shippingbox.jpg') }}" alt="boxes">
- </div>
-
-        <div class="product-img3">
+        </div>
+        <div class="product-img-3">
             <img src="{{ asset('img/airfreight.jpg') }}" alt="airfreight">
-</div>
-
-        <div class="product-img4">
+        </div>
+        <div class="product-img-4">
             <img src="{{ asset('img/ENVELOPE2.jpeg') }}" alt="orange envelope">
-</div>   
-
-
+        </div>
     </div>
 </div>
-
-
-
 @endsection
